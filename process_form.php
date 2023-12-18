14dc2f230e90f8abd61bd1ec07f308aaf990fc65
“Ashutosh <adityaashutosh06@gmail.com>
2023-12-18 12:21:37 +0530

    updated with changes in column of table

--- process_form.php
+++ process_form.php
@@ -19,8 +19,19 @@ if ($conn->connect_error) {
 $name = $_POST['name'];
 $age = $_POST['age'];
 $batch = $_POST['batch'];
+$gender = $_POST['gender'];
+$sdate= $_POST['sdate'];
 
-$sql = "INSERT INTO yoga_classes (name, age, batch) VALUES ('$name', '$age', '$batch')";
+$sql = "INSERT INTO yoga_classes (name, age, batch,gender,start_date) VALUES ('$name', '$age', '$batch','$gender','$sdate')";
+
+
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$cardNumber = $_POST['cardNumber'];
+$expiryDate= $_POST['expiryDate'];
+
+$sqlP = "INSERT INTO payment_information (phone, email, credit_card_number,expiry_date) VALUES ('$phone', '$email', '$cardNumber','$expiryDate')";
+
 
 ?>
 <!DOCTYPE html>
@@ -35,14 +46,24 @@ $sql = "INSERT INTO yoga_classes (name, age, batch) VALUES ('$name', '$age', '$b
             background-color: lightgrey; /* Change this to the desired background color */
             margin-top: 15%;
         }
+        .card{
+            max-width: 60%;
+            margin: 1% auto;
+            background-color: #fff;
+            opacity: 1;
+            background: radial-gradient(circle at 0% 100%, #3be241bb 35%, rgba(255, 255, 255, 0.671) 36%, transparent 36%), radial-gradient(circle at 100% 0%, #3be241bb 35%, rgba(255, 255, 255, 0.767) 36%, #ffffffce 36%);
+            padding: 10px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
     </style>
 </head>
 
 <body>
-
+<div class="card">
     <?php
 
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) === TRUE && $conn->query($sqlP) === TRUE) {
 
     $userDetails = ['name' => $name, 'age' => $age];
     $paymentDetails = ['amount' => 50];
@@ -52,6 +73,7 @@ if ($conn->query($sql) === TRUE) {
     // echo json_encode(['success' => $paymentResponse['success'], 'message' => $paymentResponse['message']]);
     if ($paymentResponse['success'] == true) {
         echo "<h1 style='color: green;text-align: center;'>Payment Successful</h1>";
+        echo "<h2 style='color: green;text-align: center;'>You can start joining classes from $sdate</h2>";
     } else {
         echo "<h1 style='color: red;text-align: center;'>Payment is not Successful</h1>";
     }
@@ -62,7 +84,7 @@ echo '<div style="text-align: center; margin-top: 20px;"><a href="./index.html" 
 $conn->close();
 
 ?>
-
+</div>
 </body>
 
 </html>
